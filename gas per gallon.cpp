b6004d9f749bbbc5e3ec5e6e76d494a07157a589
kenyan308 <kenyan.dunkley86@myhunter.cuny.edu>
2025-02-05 14:14:34 -0500

    Update gas per gallon.cpp

--- gas per gallon.cpp
+++ gas per gallon.cpp
@@ -7,18 +7,26 @@
 using namespace std;
 int main() 
 {
-    double miles;
+   // double miles;
     double gallons;
     double cost;
     double mpg;
-    cout << "Enter the number of miles driven: ";
-    cin >> miles;
+    double price;
+    double distance;
+    
+    //cout << "Enter the number of miles driven: ";
+    //cin >> miles;
     cout << "Enter the number of gallons used: ";
     cin >> gallons;
-    mpg = miles / gallons;
-    cout << "Enter the cost per gallon: ";
-    cin >> cost;
-    cout << "The cost per 100 miles is: " << (100.0 / mpg) * cost << endl;
-    cout << "The cost per 100 miles is: " << (100.0 / mpg) * cost << endl;
+    //mpg = miles / gallons 
+    cout << "Enter the number of miles per gallons: ";
+    cin >> mpg;
+    cout << "Enter the price per gallons: ";
+    cin >> price;
+    cost = (100.0/ mpg) * price;
+    cout << "The cost per 100 miles is: " << cost << endl;
+    distance = gallons * mpg;
+    cout << "The distance per 100 miles is: " << distance << endl;
     return 0;
 }
+
